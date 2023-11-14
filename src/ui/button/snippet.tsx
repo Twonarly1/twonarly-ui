5f0f143821bdd0d54888be325f60683b25fd2fe7
Beau Hawkinson <72956780+Twonarly1@users.noreply.github.com>
2023-11-14 14:45:54 -0600

    chore: modify read me and license md

--- src/ui/button/snippet.tsx
+++ src/ui/button/snippet.tsx
@@ -1,7 +1,9 @@
-import { ark, type HTMLArkProps } from "@ark-ui/react";
+import { ark } from "@ark-ui/react";
 import { VariantProps } from "tailwind-variants";
-import { styled } from "lib/util/styled";
 import { buttonStyles } from "./recipe";
+import { styled } from "lib/util";
+
+import type { HTMLArkProps } from "@ark-ui/react";
 
 type ButtonVariantProps = VariantProps<typeof buttonStyles>;
 export type ButtonProps = ButtonVariantProps & HTMLArkProps<"button">;
